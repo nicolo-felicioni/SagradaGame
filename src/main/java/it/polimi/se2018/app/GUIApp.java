757908c5f55f6a99e9a6b37675819ffdd65546fb
gao <10496386@polimi.it>
2018-06-15 21:38:45 +0200

    GUIApp fix

--- src/main/java/it/polimi/se2018/app/GUIApp.java
+++ src/main/java/it/polimi/se2018/app/GUIApp.java
@@ -4,6 +4,6 @@ import it.polimi.se2018.view.gui.fxmlController.GUIController;
 
 public class GUIApp {
     public static void main(String[] args) {
-        (new GUIController()).start(args);
+        (new GUIController()).start();
     }
 }
