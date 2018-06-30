022d2cd1213b769bee1d9966baad92724e9c0240
gao <10496386@polimi.it>
2018-06-30 18:12:16 +0200

    fxml e controller della schermata di gioco

---
+++ src/main/java/it/polimi/se2018/view/gui/fxmlController/GUIGame.java
@@ -0,0 +1,40 @@
+package it.polimi.se2018.view.gui.fxmlController;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.Pane;
+
+import java.awt.event.ActionEvent;
+
+public class GUIGame {
+    @FXML
+    Pane draftpool;
+    @FXML
+    Pane roundtrack;
+    @FXML
+    Pane windowpattern;
+    @FXML
+    Pane toolcardLeft;
+    @FXML
+    Pane toolcardCenter;
+    @FXML
+    Pane toolcardRight;
+    @FXML
+    Pane publiccardLeft;
+    @FXML
+    Pane publiccardCenter;
+    @FXML
+    Pane publiccardRight;
+
+    public void endTurn(javafx.event.ActionEvent event) {
+
+    }
+
+    public void privateObjectCard(javafx.event.ActionEvent event) {
+    }
+
+    public void useToolCard(javafx.event.ActionEvent event) {
+    }
+
+    public void showEnemyWindowPattern(javafx.event.ActionEvent event) {
+    }
+}
