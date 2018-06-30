a7d13cf9be7031d24b186365c505e632c77dda48
gao <10496386@polimi.it>
2018-06-30 10:45:26 +0200

    public object card and some fxml

---
+++ src/main/java/it/polimi/se2018/view/gui/fxmlController/GUIChooseWindowPattern.java
@@ -0,0 +1,12 @@
+package it.polimi.se2018.view.gui.fxmlController;
+
+import it.polimi.se2018.model.WindowPattern;
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+
+public class GUIChooseWindowPattern {
+    WindowPattern[] windowPatterns;
+    @FXML
+    GridPane gridPane;
+
+}
